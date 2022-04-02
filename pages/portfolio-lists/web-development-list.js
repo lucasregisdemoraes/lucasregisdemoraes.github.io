4bd1bf71ee66f309986ebc06dd47669deb1971b9
Lucas Regis de Moraes <LucasGRM03@gmail.com>
2022-04-02 11:12:50 -0300

    add web development portfolio list

--- pages/portfolio-lists/web-development-list.js
+++ pages/portfolio-lists/web-development-list.js
@@ -1,32 +1,201 @@
 export default [
     {
-        title: "gfdgdsaf sddasfsdsdf",
-        description: "sladkfasdfsdlfjkd",
-        link: "https://github.com/",
-        previewImage: "./assets/react-native-assets/teste.png",
+        title: "Blogr",
+        description: "Landing Page de uma plataforma de publicação",
+        link: "https://lucasregisdemoraes.github.io/Blogr-landing-page/",
+        previewImage: "./assets/web-development-assets/Blogr-landing-page.png",
         tags: [
             {
-                link: "https://github.com/",
-                abbrTitle: "Link para o projeto no GitHub",
+                link: "https://lucasregisdemoraes.github.io/Blogr-landing-page/",
+                abbrTitle: "Página na internet",
+                abbrContent: "Página"
+            },
+            {
+                link: "https://github.com/lucasregisdemoraes/Blogr-landing-page",
+                abbrTitle: "Página do projeto no GitHub",
                 abbrContent: "GitHub"
             },
             {
-                link: "https://github.com/",
-                abbrTitle: "Inspirado em projeto do Youtube",
-                abbrContent: "YouTube"
+                link: "https://www.frontendmentor.io/",
+                abbrTitle: "Baseado em um projeto do Frontend Mentor",
+                abbrContent: "Frontend Mentor"
+            }
+        ]
+    },
+    {
+        title: "DevChallenge - Desafios Concluídos",
+        description: "Desafios do DevChallenge concluídos por mim ",
+        link: "https://lucasregisdemoraes.github.io/DevChallenge-desafios-concluidos/",
+        previewImage: "./assets/web-development-assets/DevChallenge-desafios-concluidos.png",
+        tags: [
+            {
+                link: "https://lucasregisdemoraes.github.io/DevChallenge-desafios-concluidos/",
+                abbrTitle: "Página na internet",
+                abbrContent: "Página"
             },
             {
-                link: "https://github.com/",
-                abbrTitle: "Link para a página do projeto",
+                link: "https://github.com/lucasregisdemoraes/DevChallenge-desafios-concluidos",
+                abbrTitle: "Página do projeto no GitHub",
                 abbrContent: "GitHub"
             },
             {
-                abbrTitle: "Link para a página do projeto",
+                link: "https://www.devchallenge.com.br",
+                abbrTitle: "Baseado em projetos do DevChallenge",
+                abbrContent: "DevChallenge"
+            }
+        ]
+    },
+    {
+        title: "Kenziegram",
+        description: "Clone do Instagram",
+        link: "https://lucasregisdemoraes.github.io/kenziegram/",
+        previewImage: "./assets/web-development-assets/Kenziegram.png",
+        tags: [
+            {
+                link: "https://lucasregisdemoraes.github.io/kenziegram/",
+                abbrTitle: "Página na internet",
+                abbrContent: "Página"
+            },
+            {
+                link: "https://github.com/lucasregisdemoraes/kenziegram",
+                abbrTitle: "Página do projeto no GitHub",
                 abbrContent: "GitHub"
             },
             {
-                abbrTitle: "Link para a página do projeto",
+                abbrTitle: "Baseado em um evento da Kenzie Academy",
+                abbrContent: "Kenzie Academy"
+            }
+        ]
+    },
+    {
+        title: "BeautySalon",
+        description: "Landing Page para um salão de beleza",
+        link: "https://lucasregisdemoraes.github.io/beauty-salon/",
+        previewImage: "./assets/web-development-assets/Beautysalon.png",
+        tags: [
+            {
+                link: "https://lucasregisdemoraes.github.io/beauty-salon/",
+                abbrTitle: "Página na internet",
+                abbrContent: "Página"
+            },
+            {
+                link: "https://github.com/lucasregisdemoraes/beauty-salon",
+                abbrTitle: "Página do projeto no GitHub",
                 abbrContent: "GitHub"
+            },
+            {
+                abbrTitle: "Baseado em um evento da Rocketseat",
+                abbrContent: "Rocketseat"
+            }
+        ]
+    },
+    {
+        title: "Certificard",
+        description: "Certificado de conclusão da imersão dev - Alura",
+        link: "https://lucasregisdemoraes.github.io/certificard/",
+        previewImage: "./assets/web-development-assets/Certificard.png",
+        tags: [
+            {
+                link: "https://lucasregisdemoraes.github.io/certificard/",
+                abbrTitle: "Página na internet",
+                abbrContent: "Página"
+            },
+            {
+                link: "https://github.com/lucasregisdemoraes/certificard",
+                abbrTitle: "Página do projeto no GitHub",
+                abbrContent: "GitHub"
+            },
+            {
+                abbrTitle: "Baseado na imersão dev da Alura",
+                abbrContent: "Alura"
+            }
+        ]
+    },
+    {
+        title: "Spotify",
+        description: "Clone do Spotify",
+        link: "https://lucasregisdemoraes.github.io/spotify-clone/",
+        previewImage: "./assets/web-development-assets/Spotify.png",
+        tags: [
+            {
+                link: "https://lucasregisdemoraes.github.io/spotify-clone/",
+                abbrTitle: "Página na internet",
+                abbrContent: "Página"
+            },
+            {
+                link: "https://github.com/lucasregisdemoraes/spotify-clone",
+                abbrTitle: "Página do projeto no GitHub",
+                abbrContent: "GitHub"
+            },
+            {
+                abbrTitle: "Um projeto próprio",
+                abbrContent: "Projeto Próprio"
+            }
+        ]
+    },
+    {
+        title: "dev.finance$",
+        description: "Site para gerenciamento das finanças",
+        link: "https://lucasregisdemoraes.github.io/devfinances/",
+        previewImage: "./assets/web-development-assets/DevFinances.png",
+        tags: [
+            {
+                link: "https://lucasregisdemoraes.github.io/devfinances/",
+                abbrTitle: "Página na internet",
+                abbrContent: "Página"
+            },
+            {
+                link: "https://github.com/lucasregisdemoraes/devfinances",
+                abbrTitle: "Página do projeto no GitHub",
+                abbrContent: "GitHub"
+            },
+            {
+                abbrTitle: "Baseado em um evento da Rocketseat",
+                abbrContent: "Rocketseat"
+            }
+        ]
+    },
+    {
+        title: "Huddle",
+        description: "Landing Page de uma plataforma para criação de comunidades",
+        link: "https://lucasregisdemoraes.github.io/huddle-landing-page/",
+        previewImage: "./assets/web-development-assets/Huddle.png",
+        tags: [
+            {
+                link: "https://lucasregisdemoraes.github.io/huddle-landing-page/",
+                abbrTitle: "Página na internet",
+                abbrContent: "Página"
+            },
+            {
+                link: "https://github.com/lucasregisdemoraes/huddle-landing-page",
+                abbrTitle: "Página do projeto no GitHub",
+                abbrContent: "GitHub"
+            },
+            {
+                abbrTitle: "Baseado em um projeto do Frontend Mentor",
+                abbrContent: "Frontend Mentor"
+            }
+        ]
+    },
+    {
+        title: "rocket.q",
+        description: "Plataforma para gerenciamento de perguntas",
+        link: "https://lucasregisdemoraes.github.io/NLW6-rocketq/",
+        previewImage: "./assets/web-development-assets/RocketQ.png",
+        tags: [
+            {
+                link: "https://lucasregisdemoraes.github.io/NLW6-rocketq/",
+                abbrTitle: "Página na internet",
+                abbrContent: "Página"
+            },
+            {
+                link: "https://github.com/lucasregisdemoraes/NLW6-rocketq",
+                abbrTitle: "Página do projeto no GitHub",
+                abbrContent: "GitHub"
+            },
+            {
+                abbrTitle: "Baseado em um evento da Rocketseat",
+                abbrContent: "Rocketseat"
             }
         ]
     }
